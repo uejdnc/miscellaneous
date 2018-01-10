c23208c79d3840a0b1d6e438a7b460adbf15f33a
uejdnc <uejdnc@gmail.com>
2018-01-10 17:31:22 -0600

    added date object to string function

--- miscellaneous.js
+++ miscellaneous.js
@@ -48,5 +48,9 @@ String.prototype.tDate = function(i, o) {
 	if (i) date.setDate(date.getDate() + i);
 	return o ? date : date.getFullYear() + '-' + String(date.getMonth() + 1).padStart(2, 0) + '-' + String(date.getDate()).padStart(2, 0);
 };
+/*Date to string*/
+Date.prototype.DtoS = function() {
+	return this.getFullYear() + '-' + String(this.getMonth() + 1).padStart(2, 0) + '-' + String(this.getDate()).padStart(2, 0);
+}
 /*If is a valid date*/
 String.prototype.iDate = function(i, o) { return new Date(this) != 'Invalid Date' };
