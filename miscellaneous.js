aa1eed2fc85c2d546b4b55e32a443b7b6acafc7b
uejdnc <uejdnc@gmail.com>
2018-01-25 10:29:24 -0600

    Added Ajax functionality

--- miscellaneous.js
+++ miscellaneous.js
@@ -108,6 +108,20 @@ var QS = (e, p) => (p ? p : document).querySelector(e),
 		}
 
 		return chartelem;
+	},
+	ajax = (d) => {
+		let xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+			(d.done || (() => ''))(this);
+		};
+
+		xhttp.open(d.method || 'POST', d.url, d.async || true);
+		xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+		if (d.data) {
+			let data = '';
+			for (key in d.data) data += encodeURIComponent(key) + "=" + encodeURIComponent(d.data[key]) + "&";
+			xhttp.send(data);
+		}
 	};
 
 /*Append or prepend data to elemet*/
