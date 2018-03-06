aff6e925bf60de6bc743329436749c3dd7c337f3
Fernando Sedano <uejdnc@gmail.com>
2018-03-06 11:41:51 -0600

    Mayor update
    Added tons of functions like creating an HTML element from a string and replacing an element and all sort of stuff

--- miscellaneous.js
+++ miscellaneous.js
@@ -1,16 +1,24 @@
 /*
-QS = Shortcut of querySelector
-QSA = Shortcut of querySelectorAll
-today = Today's date on string
-diffDate = Difference in days from two strings
-isJSON = If string is valid JSON
-bTime = Beautify Time
-oSumE = Sum elements in object
+	QS = Shortcut of querySelector
+	QSA = Shortcut of querySelectorAll
+	today = Today's date on string
+	diffDate = Difference in days from two strings
+	isJSON = If string is valid JSON
+	bTime = Beautify Time
+	oSumE = Sum elements in object
+	maxV = Get max value of JSON
 */
-var QS = (e, p) => (p ? p : document).querySelector(e),
-	QSA = (e, p) => (p ? p : document).querySelectorAll(e),
-	today = () => { return new Date().toJSON().slice(0, 10) },
-	oSumE = (o) => { return Object.values(o).reduce((a, b) => parseInt(a) + parseInt(b)) },
+var QS = (e, p) => (p || document).querySelector(e),
+	QSA = (e, p) => (p || document).querySelectorAll(e),
+	QID = (e, p) => (p || document).getElementById(e),
+	today = () => new Date().toJSON().slice(0, 10),
+	oSumE = (o) => Object.values(o).reduce((a, b) => parseInt(a) + parseInt(b)),
+	cssVar = (v, e = QS(':root')) => getComputedStyle(e).getPropertyValue(`--${v}`),
+	maxV = (array, key) => {
+		let max = 0;
+		for (let i in array) max = (array[i][key] >= max) ? array[i][key] : max;
+		return max;
+	},
 	bTime = t => {
 		t = Math.round(parseInt(isNaN(t) ? 0 : t));
 		return (((t > 59) ? `${parseInt(t / 60)}h ` : '') + ((t % 60) ? `${(t % 60)}'` : '')).trim() || `${t}'`;
@@ -33,7 +41,6 @@ var QS = (e, p) => (p ? p : document).querySelector(e),
 	sLineChart = (data, orientation, container, sum = 0, dd) => {
 		let lines = '',
 			max = data.length;
-
 
 		if (!orientation) orientation = 'v';
 		if (!container) container = 'body';
@@ -109,27 +116,58 @@ var QS = (e, p) => (p ? p : document).querySelector(e),
 
 		return chartelem;
 	},
-	ajax = (d) => {
-		let xhttp = new XMLHttpRequest(),
-			contentType = (d.contentType == undefined) ? true : d.contentType,
+	ajax = d => {
+		if (!d) d = {};
+		let xhttp = d.beacon || new XMLHttpRequest(),
+			contentType = d.contentType === undefined || d.contentType,
 			data = (contentType ? new URLSearchParams(new FormData(d.form)).toString() : d.form) || '';
-		xhttp.onreadystatechange = function() { if (this.readyState == 4 && this.status == 200)(d.done || (() => ''))(this.responseText) };
 
-		xhttp.open(d.method || 'POST', d.url, d.async || true);
-		if (contentType) xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 		if (d.data && !data) { for (key in d.data) data += encodeURIComponent(key) + "=" + encodeURIComponent(d.data[key]) + "&" }
 
-		xhttp.send(data);
-	};
+		if (d.beacon == true) navigator.sendBeacon(d.url, data) && ((typeof d.callback == 'function') && d.callback());
+		else {
+			xhttp.onreadystatechange = function() { if (this.readyState == 4 && this.status == 200)(d.done || (() => ''))(this.responseText) };
+			xhttp.onerror = () => d.retry && ((typeof d.retry == 'function') ? d.retry : ajax)(d);
+
+			xhttp.open(d.method || 'POST', d.url, d.async === undefined || d.async);
+			if (contentType) xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+			xhttp.send(data);
+		}
+		return xhttp;
+	},
+	post = (url, data, callback, r) => ajax({ url: url, data: data, done: callback, retry: r }),
+	beacon = (url, data, callback) => ajax({ url: url, data: data, beacon: true, done: callback });
 
 /*Append or prepend data to elemet*/
-Node.prototype.paste = function(t, p) { this.insertAdjacentHTML(p ? 'afterbegin' : 'beforeend', t) };
-/*Get attribute to element with fallback*/
+Node.prototype.paste = function(t, p) {
+	let pos = ['beforeEnd', 'afterBegin', 'beforeBegin', 'afterEnd'];
+
+	this.insertAdjacentHTML(pos[p || 0], t);
+	return this;
+};
+/*Append or prepend data to elemet*/
+Node.prototype.iHTML = function(e) {
+	if (e) {
+		this.innerHTML = '';
+		if (typeof e == 'string') this.innerHTML = e;
+		else this.appendChild(e);
+	}
+	return e ? this : this.innerHTML;
+};
+/*Get attribute of element with fallback*/
 Node.prototype.gAttr = function(a, f) { return (this.attributes[a]) ? this.attributes[a].nodeValue : (f ? ((this.attributes[f]) ? this.attributes[f].nodeValue : undefined) : undefined); };
-/*Set attribute to element*/
+/*Set attribute of element*/
 Node.prototype.sAttr = function(a, v) {
 	if (this.attributes[a]) this.attributes[a].nodeValue = v;
 	else this.setAttribute(a, v);
+	return this;
+};
+/*Remove attribute of element*/
+Node.prototype.rAttr = function(a) {
+	let that = this;
+	a.split(' ').forEach(e => that.removeAttribute(e));
+	return this;
 };
 /*Has class shortcut*/
 Node.prototype.hClass = function(c, t) {
@@ -139,44 +177,94 @@ Node.prototype.hClass = function(c, t) {
 	return t ? cond.t : !cond.f;
 };
 /*Add class shortcut*/
-Node.prototype.aClass = function(c) { this.classList.add(...c.split(' ')) };
+Node.prototype.aClass = function(c) {
+	this.classList.add(...c.split(' '));
+	return this;
+};
 /*Remove class shortcut*/
-Node.prototype.rClass = function(c) { this.classList.remove(...c.split(' ')) };
+Node.prototype.rClass = function(c) {
+	this.classList.remove(...c.split(' '));
+	return this;
+};
 /*Switch class shortcut*/
-Node.prototype.sClass = function(r, a, s) { this.classList.remove(...(s ? a : r).split(' ')), this.classList.add(...(s ? r : a).split(' ')) };
+Node.prototype.sClass = function(r, a, s) {
+	this.rClass(s ? a : r), this.aClass(s ? r : a);
+	return this;
+};
 /*Toggle class shortcut*/
 Node.prototype.tClass = function(c, t) {
-	let cond = (t === undefined) ? this.hClass(c) : !t;
-	if (cond) this.rClass(c);
-	else this.aClass(c);
+	(((t === undefined) ? this.hClass(c) : !t) ? c => this.rClass(c) : c => this.aClass(c))(c);
+	return this;
 };
 /*Add class shortcut to NodeList*/
-NodeList.prototype.maClass = function(c) { this.forEach((e) => e.aClass(c)) };
+NodeList.prototype.maClass = function(c) {
+	this.forEach((e) => e.aClass(c));
+	return this;
+};
 /*Remove class shortcut to NodeList*/
-NodeList.prototype.mrClass = function(c) { this.forEach((e) => e.rClass(c)) };
+NodeList.prototype.mrClass = function(c) {
+	this.forEach((e) => e.rClass(c));
+	return this;
+};
 /*Switch class shortcut to NodeList*/
-NodeList.prototype.msClass = function(r, a, s) { this.forEach((e) => e.sClass(r, a, s)) };
+NodeList.prototype.msClass = function(r, a, s) {
+	this.forEach((e) => e.sClass(r, a, s));
+	return this;
+};
 /*Toggle class shortcut to NodeList*/
-NodeList.prototype.mtClass = function(c, t) { this.forEach((e) => e.tClass(c, t)) };
+NodeList.prototype.mtClass = function(c, t) {
+	this.forEach((e) => e.tClass(c, t));
+	return this;
+};
+/*Get attribute of nodeList with fallback*/
+NodeList.prototype.mgAttr = function(a, f) { return Array.from(this).map(e => e.gAttr(a, f)) };
+/*Get ids of nodeList with fallback*/
+NodeList.prototype.ids = function(a, f) { return Array.from(this).map(e => e.id) };
+/*NodeList multifunction to array*/
+NodeList.prototype.multiArray = function(f, p) { return Array.from(this).map(e => p ? e[f](...p) : e[f]) };
 /*Set attribute to NodeList*/
-NodeList.prototype.msAttr = function(a, v) { this.forEach(e => e.sAttr(a, v)) };
+NodeList.prototype.msAttr = function(a, v) {
+	this.forEach(e => e.sAttr(a, v));
+	return this;
+};
+/*Remove attribute to NodeList*/
+NodeList.prototype.mrAttr = function(a, v) {
+	this.forEach(e => e.rAttr(a, v));
+	return this;
+};
 /*Remove elements from NodeList*/
-NodeList.prototype.removes = function(c) { this.forEach(i => i.remove()) };
+NodeList.prototype.removes = function(c) { for (let e of this) e.remove() };
 /*Add onclick to elements from NodeList*/
-NodeList.prototype.oClick = function(c) { this.forEach(i => i.onclick = c) };
+NodeList.prototype.oClick = function(c) {
+	for (let e of this) e.onclick = c
+	return this;
+};
 /*Add mouseenter and mouseleave to elements from NodeList*/
-NodeList.prototype.oHover = function(e, l) { this.forEach((i) => { i.onmouseenter = e, i.onmouseleave = l || e; }) };
+NodeList.prototype.oHover = function(e, l) {
+	for (let i of this) i.onmouseenter = e, i.onmouseleave = l
+	return this;
+};
 /*Multiple events (click, input, etc) to Node*/
-Node.prototype.oevent = function(e, f) {
+Node.prototype.oevent = function(e, s, f2) {
 	let that = this,
+		clase = s,
+		funcion = (typeof s == 'function') ? s : f2,
 		events = e.trim().split(' ');
-	for (let i in events) that[`on${events[i]}`] = f;
+
+	for (let i of events) that[`on${i}`] = (clase == funcion) ? funcion : function(e) { if (clase == funcion || e.target.closest(clase)) funcion(e) };
+	return this;
 }
 /*Multiple events (click, input, etc) to NodeList*/
-NodeList.prototype.oEvent = function(e, f) {
-	let events = e.trim().split(' ');
-	this.forEach((ele) => { for (let i in events) ele[`on${events[i]}`] = f });
+NodeList.prototype.oEvent = function(e, s, f) {
+	for (let i of this) i.oevent(e, s, f)
+	return this;
 }
+/*Set time out alternative*/
+Node.prototype.wait = function(t) {
+	let before = Date.now();
+	while (Date.now() < before + t) {};
+	return this;
+};
 /*Add/Subtract days from string or object*/
 String.prototype.tDate = function(i, o) {
 	let date = new Date(this.split('-'));
@@ -189,3 +277,60 @@ Date.prototype.DtoS = function() { return this.getFullYear() + '-' + String(this
 String.prototype.iDate = function(i, o) { return new Date(this) != 'Invalid Date' };
 /*Get element index*/
 Node.prototype.gIndex = function() { return Array.prototype.slice.call(this.parentElement.children).indexOf(this) };
+/*Clone node*/
+Node.prototype.cloneN = function(cEvents = true) {
+	let that = this,
+		clone = that.cloneNode(false);
+
+	if (cEvents) {
+		for (let i in that) {
+			if (/on/i.exec(i)) clone[i] = (/on/i.exec(i).index == 0) ? that[i] : clone[i];
+		}
+
+		if (that.childNodes.length) {
+			[].slice.call(that.childNodes).forEach(function(that) {
+				clone.appendChild(that.cloneN());
+			});
+		}
+	}
+
+	return clone;
+};
+/*Paste page into node with ajax*/
+Node.prototype.loadP = function(url, d) {
+	let that = this;
+	ajax({
+		url: url,
+		async: false,
+		data: d || undefined,
+		method: d ? 'POST' : 'GET',
+		done: data => that.innerHTML = data
+	});
+	return this;
+};
+/*Replace element with an HTML string*/
+String.prototype.createHTML = function() { return document.createRange().createContextualFragment(this) }
+/*Replace element with an HTML string*/
+Node.prototype.replaceElement = function(s) {
+	this.replaceWith((typeof s == 'string') ? s.createHTML() : s);
+}
+/*Next element*/
+Node.prototype.nextE = function(p = 1) {
+	let e = this;
+	try {
+		for (let i = 0; i < p; i++) e = e.nextElementSibling;
+	} catch (e) {
+		return undefined;
+	}
+	return e;
+}
+/*Prev element*/
+Node.prototype.prevE = function(p = 1) {
+	let e = this;
+	try {
+		for (let i = 0; i < p; i++) e = e.previousElementSibling;
+	} catch (e) {
+		return undefined;
+	}
+	return e;
+}
